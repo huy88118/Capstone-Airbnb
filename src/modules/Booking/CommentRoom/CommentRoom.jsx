c92cb471fff3c4f32afde1fc7b1664b3faff26a3
Huy Nguyễn <119769127+huy88118@users.noreply.github.com>
2023-06-19 22:22:05 +0700

    css comment room

--- src/modules/Booking/CommentRoom/CommentRoom.jsx
+++ src/modules/Booking/CommentRoom/CommentRoom.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 // import {apiCommentRoom} from "../../../apis/locationAPI"
 import { apiGetComment } from "../../../apis/locationAPI";
+import styles from "./CommentRoom.module.scss"
 
 function CommentRoom({ id }) {
   const [comments, setComments] = useState([]);
@@ -22,13 +23,21 @@ function CommentRoom({ id }) {
 
   if (error) return null;
   return (
-    <div>
+    <div className={styles.comment}>
+       <h2>Bình luận của khách hàng đã đến đây !</h2>
       {comments.map((item) => {
         if (item.maPhong.toString() === id) {
           return (
-            <div>
-              <p key={item.maPhong}>Nội dung: {item.noiDung}</p>
-            </div>
+          <div className="col-sm-4">
+               <div className={styles.binhLuan} key={item.maPhong}>
+              
+              <span className={styles.commentId}>Khách hàng<p className={styles.commentP}>{item.id}</p></span>
+              <p>Nội dung : {item.noiDung}</p>
+              <p >Ngày bình luận :{item.ngayBinhLuan}</p>
+        </div>
+          </div>   
+
+         
           );
         }
       })}
